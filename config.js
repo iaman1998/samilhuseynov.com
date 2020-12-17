ef0a3c906bef3bae07aef6f8bf6fd6ba4ebc5aa5
Texnocom <76031213+Texnocom@users.noreply.github.com>
2020-12-17 21:49:56 +0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+export default {
+  tgUsername: "Samil",
+  githubUsername: "Texnocom",
+  websiteName: "@BotMap",
+  heroHead: "Xoş Gəldin !!",
+  heroText: "Mənim Adım Şamil! ",
+  facebookUsername: "samilhuseynovv",
+  instaUsername: "milliportalaz",
+  typedArray : ["Developer","Desinger","TeleBot"]
+};
