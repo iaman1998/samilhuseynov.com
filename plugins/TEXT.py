647e884fa6f7434f6118d966530d953f2c72ac71
Texnocom <76031213+Texnocom@users.noreply.github.com>
2020-12-17 21:52:25 +0400

    Add files via upload

---
+++ plugins/TEXT.py
@@ -0,0 +1,70 @@
+
+
+# Here is Your Drive Folder Name You can Replace it with Your Desire name(Optional)
+drive_folder_name = "GDriveUploaderBot"
+
+
+# Enter Your Mega email And Pass (Required)
+MEGA_EMAIL = "samilhuseynovv@gmail.com"
+MEGA_PASSWORD = "samilelman0"
+
+
+START = " Salam {}  \nMən Drive Yükləyici Botuyam. Zəhmət olmasa məni istifadə etmək üçün /auth istifadə edərək icazə verin.\n \n Daha çox məlumat üçün /help  \n\n Üçüncü Tərəf Veb Saytı \n Dəstək əlavə edildi\n\n Bot Yeniləmələri üçün /update \n <a href ='https://t.me/BotMaps'>Kanalımıza Buyur</a>\nHər Hansısa iradı bildir  @samil"
+
+HELP = """   <b>TƏSDİQLƏ</b> 
+       /auth isdifadə edərək Goodle-driverdən aldığın
+       okodu bota göndər 
+<b> Girişinizi dəyişdirmək istəyirsiniz
+        Hesab ?</b> \n
+        İstifadə edə bilərsən /revoke 
+        əmr          
+<b>Bu botla nə edə bilərəm?</b>
+            Hərhansı bir linklə istədiyini 
+            yükləyə bilərsan google-driver 
+            hesabınız ilə
+<b> Bot tərəfindən dəstəklənən bağlantılar</b>
+            * Birbaşa bağlantılar
+            * Açıq yükləmə əlaqələri [Max Speed
+              500 KBps :(]
+            * Dropbox əlaqələri
+            * Mega əlaqələri
+            
+            + Daha çoxu yolda :)
+                
+Xəta Hesabatı @Samil
+        """
+DP_DOWNLOAD = "Dropbox Linki !! Endirmə başladı ..."
+OL_DOWNLOAD = "Openload Link !! Endirməyə Başlandı ... \n Açıq Yükləmə Linksı Çox Yavaşdır"
+PROCESSING = "İstəyiniz işlənir...!!"
+DOWN_TWO = True
+DOWNLOAD = "Endirmə başladı..."
+DOWN_MEGA = "Yüklənməyə başladı ... \n Mega Links \n Çox Yavaşdır :( "
+DOWN_COMPLETE = "Yükləmə tamamlandı !!"
+NOT_AUTH = "Bu Botdan istifadə etmək səlahiyyətiniz yoxdur \n \n Zəhmət olmasa mənə /auth istifadə edərək icazə verin  \n\n @Samil"
+REVOKE_FAIL = "Onsuzda icazəsizsiniz \n. Xahiş edirəm Avtorizasiya etmək üçün /auth istifadə edin @Samil "
+AUTH_SUCC = "Müvəffəqiyyətlə səlahiyyətli !! \n \n İndi mənə birbaşa əlaqə göndərin:)"
+ALREADY_AUTH = "Zaten yetkilisiniz! \n \n Disk Hesabını Dəyişdirmək İstəyirsiniz? \n \n İstifadə et / ləğv et \n \n Hesabat At @Samil "
+AUTH_URL = '<a href ="{}">Vist This Url</a> \n Generate And Copy Your Google Drive Token And Send It To Me'
+UPLOADING = "Tamam Yükləyin !! \n Faylınız yüklənir "
+REVOKE_TOK = " Nişanınız Uğurla ləğv edildi !! \n \n Drive Hesabınızı Yenidən Avtorizasiya etmək üçün /auth istifadə edin. "
+# DOWN_PATH = "Downloads\\" #windows path
+DOWN_PATH = "Downloads/"  # Linux path
+DOWNLOAD_URL = "Faylınız Uğurla Yükləndi \n\n <b>Fayl adı</b> : {} \n\n <b> Ölçü</b> : {} MB \n\n <b>Download</b> {}"
+AUTH_ERROR = "AUTH səhv !! Zəhmət olmasa mənə etibarlı bir nişan göndərin və ya yenidən səlahiyyət verin  \n\n hesabat At @Samil"
+OPENLOAD = True
+DROPBOX = True
+MEGA = True
+
+
+UPDATE = """ <b> 27.07.2019 tarixində yeniləmə </b>
+            * MEGA LINK əlavə edildi
+            * Xəta İdarəetmə Təkmilləşdirildi
+
+<b> Bot tərəfindən dəstəklənən bağlantılar </b>
+            * Birbaşa Bağlantılar
+            * Openload əlaqələri [Maksimum Sürət
+              500 KBps :(]
+            * Dropbox əlaqələri
+            * Mega bağlantılar (yalnız fayllar)
+            
+            + Daha çoxu yolda :) "" "
